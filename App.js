2dcd16f7d6c9c45d32bc2b8afb7caa466678552d
root <alexsouzasilvax@gmail.com>
2020-05-02 01:58:46 -0300

    cardNotificacao, cardSaldo

--- App.js
+++ App.js
@@ -1,10 +1,139 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import IconFontAwesome from "react-native-vector-icons/FontAwesome";
+import IconIonicons from "react-native-vector-icons/Ionicons";
+import IconSimpleLineIcons from "react-native-vector-icons/SimpleLineIcons";
+import IconEntypo from "react-native-vector-icons/Entypo";
+import icon from "./assets/santander.png";
 
 export default function App() {
+  const [visibleSaldo, setVisibleSaldo] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content" />
+      <ScrollView style={styles.scrollView}>
+        {/* header */}
+        <View style={styles.viewHeader}>
+          <View style={styles.flexDirectionRow}>
+            <TouchableOpacity style={styles.btnIconLeft}>
+              <IconFontAwesome
+                name={"bell-o"}
+                size={16}
+                style={styles.iconSino}
+              />
+            </TouchableOpacity>
+            <View style={styles.viewIconCenter}>
+              <Image source={icon} style={styles.iconSantander} />
+            </View>
+            <TouchableOpacity style={styles.btnIconRigth}>
+              <IconIonicons
+                name={"ios-menu"}
+                size={24}
+                style={styles.iconSino}
+              />
+            </TouchableOpacity>
+          </View>
+
+          {/* text name anda conta */}
+          <View style={styles.viewNomeConta}>
+            <Text style={styles.textOlaNome}>Olá, Alex</Text>
+            <Text style={styles.textAgCc}>Ag 1234 Cc 01020304-5</Text>
+          </View>
+        </View>
+
+        {/* card notificacao */}
+        <View style={styles.cardNotificacao}>
+          <View style={[styles.flexDirectionRow, styles.viewCardNotificacao]}>
+            <View style={styles.viewIconCardNotificacao}>
+              <IconSimpleLineIcons
+                name={"lock"}
+                size={25}
+                style={styles.iconCardNotificacao}
+              />
+            </View>
+            <View style={styles.viewTextCardNotificacao}>
+              <Text style={styles.textCardNotificacao}>
+                Habilite o ID Santander e faça pagamentos e outras transações
+                pelo app
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        {/* card saldo */}
+        {visibleSaldo ? (
+          <TouchableOpacity
+            style={[styles.cardNotificacao, styles.cardSaldo]}
+            onPress={() => setVisibleSaldo(!visibleSaldo)}
+          >
+            <View style={[styles.flexDirectionRow, styles.viewCardSaldo]}>
+              <View style={styles.viewIconLeftCardSaldo}>
+                <IconEntypo
+                  name={"credit"}
+                  size={18}
+                  style={styles.iconLeftCardSaldo}
+                />
+              </View>
+
+              <View style={styles.viewTextCardSaldo}>
+                <Text style={styles.textCardSaldo}>Saldo disponível</Text>
+              </View>
+
+              <View style={styles.viewIconRigthCardSaldo}>
+                <IconEntypo
+                  name={"chevron-thin-down"} //chevron-thin-up
+                  size={20}
+                  style={styles.iconRigthCardSaldo}
+                />
+              </View>
+            </View>
+          </TouchableOpacity>
+        ) : (
+          <View style={[styles.cardNotificacao, styles.cardSaldo2]}>
+            <TouchableOpacity
+              style={[styles.cardSaldo, { marginTop: 0 }]}
+              onPress={() => setVisibleSaldo(!visibleSaldo)}
+            >
+              <View style={[styles.flexDirectionRow, styles.viewCardSaldo]}>
+                <View style={styles.viewIconLeftCardSaldo}>
+                  <IconEntypo
+                    name={"credit"}
+                    size={18}
+                    style={styles.iconLeftCardSaldo}
+                  />
+                </View>
+
+                <View style={styles.viewTextCardSaldo}>
+                  <Text style={styles.textCardSaldo}>Saldo disponível</Text>
+                </View>
+
+                <View style={styles.viewIconRigthCardSaldo}>
+                  <IconEntypo
+                    name={"chevron-thin-up"}
+                    size={20}
+                    style={styles.iconRigthCardSaldo}
+                  />
+                </View>
+              </View>
+            </TouchableOpacity>
+            <Text style={styles.textSaldo}>R$ 120.900,97</Text>
+            <View style={styles.divisao} />
+            <TouchableOpacity onPress={() => {}} style={styles.btnVerExtrato}>
+              <Text style={styles.textBtnVerExtrato}>Ver extrato</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -12,8 +141,153 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ec0000",
+    paddingTop: 24,
+  },
+  viewHeader: {
+    backgroundColor: "#ec0000",
+    height: 170,
+  },
+  scrollView: {
+    backgroundColor: "white",
+  },
+  iconSantander: {
+    left: 5,
+    width: 30,
+    height: 30,
+    alignSelf: "center",
+  },
+  iconSino: {
+    color: "#F3F3F3",
+    paddingTop: 8,
+  },
+  flexDirectionRow: {
+    flexDirection: "row",
+  },
+  btnIconLeft: {
+    alignSelf: "flex-start",
+    paddingStart: 18,
+  },
+  viewIconCenter: {
+    flex: 1,
+    alignSelf: "center",
+  },
+  btnIconRigth: {
+    alignSelf: "flex-end",
+    paddingEnd: 18,
+  },
+  viewNomeConta: {
+    paddingStart: 20,
+    paddingTop: 30,
+  },
+  textOlaNome: {
+    color: "#F3F3F3",
+    fontSize: 18,
+  },
+  textAgCc: {
+    paddingTop: 5,
+    color: "#F3F3F3",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  cardNotificacao: {
+    alignSelf: "center",
+    backgroundColor: "white",
+    marginTop: -30,
+    width: Dimensions.get("screen").width - 30,
+    height: 120,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#e6e6e6",
+    borderBottomWidth: 1,
+    borderTopWidth: 0,
+    borderRightWidth: 0.5,
+    borderLeftWidth: 0.5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  viewCardNotificacao: {
+    flex: 1,
+    paddingStart: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  viewIconCardNotificacao: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    borderWidth: 1.3,
+    borderColor: "#ec0000",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconCardNotificacao: {
+    color: "#ec0000",
+  },
+  viewTextCardNotificacao: {
+    flex: 1,
+    height: 80,
+    margin: 5,
+    paddingTop: 12,
+    marginStart: 10,
+  },
+  textCardNotificacao: {
+    fontSize: 16,
+    color: "#696969",
+  },
+  cardSaldo: {
+    marginTop: 30,
+    height: 50,
+  },
+  cardSaldo2: {
+    marginTop: 30,
+    height: 160,
+  },
+  viewCardSaldo: {
+    flex: 1,
+    paddingStart: 20,
+    alignItems: "center",
+  },
+  viewIconLeftCardSaldo: {},
+  iconLeftCardSaldo: {
+    color: "#444",
+  },
+  viewTextCardSaldo: {
+    paddingStart: 10,
+  },
+  textCardSaldo: {
+    color: "#444",
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  viewIconRigthCardSaldo: {
+    flex: 1,
+    alignItems: "flex-end",
+    paddingEnd: 20,
+  },
+  textSaldo: {
+    fontSize: 21,
+    fontWeight: "bold",
+    paddingStart: 20,
+    paddingTop: 15,
+  },
+  divisao: {
+    marginTop: 25,
+    alignSelf: "center",
+    width: "90%",
+    backgroundColor: "#b0b0b0",
+    height: 0.5,
+  },
+  btnVerExtrato: {
+    alignItems: "center",
+    height: 40,
+  },
+  textBtnVerExtrato: {
+    paddingTop: 12,
+    fontSize: 16,
+    color: "#ec0000",
   },
 });
